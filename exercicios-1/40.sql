2660e510efa2d42e2fb0d352d0941cecea193b58
Adriano-Silveira <73249347+Adriano-Silveira@users.noreply.github.com>
2020-10-27 21:50:08 -0300

    Update 40.sql

--- exercicios-1/40.sql
+++ exercicios-1/40.sql
@@ -1,1 +1,1 @@
-SELECT * FROM world.city LEFT JOIN world.countrylanguage on world.city.CountryCode = world.countrylanguage.CountryCode WHERE world.countrylanguage.Language = 'Dutch';
+SELECT * FROM world.city INNER JOIN world.countrylanguage on world.city.CountryCode = world.countrylanguage.CountryCode WHERE world.countrylanguage.Language = 'Dutch';
